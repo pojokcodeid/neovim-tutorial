b67e8bb53172c27b64c2cfef8b3df653c1feb006
Pojok Code <pojokcodeid@gmail.com>
2024-10-31 20:21:47 +0700

    enc: add smart-split dan coderunner

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,8 @@ return {
 	"sharpchen/Eva-Theme.nvim",
 	lazy = false,
 	priority = 1000,
+	config = function()
+		local colorscheme = "Eva-Dark"
+		vim.cmd("colorscheme " .. colorscheme)
+	end,
 }
--- lua/user/colorscheme.lua
+++
@@ -1,5 +0,0 @@
-local colorscheme = "Eva-Dark"
-local ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
-if not ok then
-	print("Color scheme '" .. colorscheme .. "' not found!")
-end
